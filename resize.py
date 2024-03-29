3f524c05551249864c15df606470a21da52a450b
micwonder <130264564+micwonder@users.noreply.github.com>
2024-03-29 10:07:05 -0500

    Replaced imageio with pillow

--- resize.py
+++ resize.py
@@ -1,4 +1,7 @@
-import os, cv2
+import os
+
+# import cv2
+from PIL import Image
 from tqdm import tqdm
 
 
@@ -18,17 +21,20 @@ def run():
         ):
             img_paths.append(img_path)
 
-    
-    
-    for i, img_path in tqdm(enumerate(img_paths), total=len(img_paths), desc="Processing Images"):
-        img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-        resized_image = cv2.resize(img, (640, 640))
+    for i, img_path in tqdm(
+        enumerate(img_paths), total=len(img_paths), desc="Processing Images"
+    ):
+        # img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+        # resized_image = cv2.resize(img, (640, 640))
 
-        target_path = os.path.join(
-            target, os.path.join(os.path.splitext(img_names[i])[0], ".jpg")
-        )
+        target_path = os.path.join(target, os.path.splitext(img_names[i])[0] + ".jpg")
 
-        cv2.imwrite(target_path, resized_image)
+        with Image.open(img_path) as img:
+            img.thumbnail((1280, 640))
+            img = img.convert("RGB")
+            img.save(target_path, "jpeg", optimize=True)
+
+        # cv2.imwrite(target_path, resized_image)
 
 
 if __name__ == "__main__":
